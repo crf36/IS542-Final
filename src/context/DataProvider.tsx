83bc7dbec2adfaa8c131fbeaf403d048744e7d14
crf36 <149890484+crf36@users.noreply.github.com>
2025-04-18 22:08:23 -0600

    fixed github pages deployment script

--- src/context/DataProvider.tsx
+++ src/context/DataProvider.tsx
@@ -1,7 +1,7 @@
 // import { useFetchMovies } from "../api/MovieListApi";
 import { ReactNode } from "react";
 import { MoviesDataContext } from "./Data";
-import { useFetchMovies } from "../MovieApi";
+import { useFetchMovies } from "../MovieAPI";
 
 export function DataProvider({ children }: { children: ReactNode }) {
   const { movies, isLoading } = useFetchMovies();
